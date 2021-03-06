d3690eb8a8a72560acdc4e8990d922a4dbce6923
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2021-03-06 18:55:24 -0500

    added photo transitions on home

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-75px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+    });
+    $(".home_works").click(function() {
+          
+        $("body,html").animate({
+            scrollTop: $(".work_box").offset().top
+        -125}, 800);
+    });
+    $(".home_about").click(function() {
+          
+        $("body,html").animate({
+            scrollTop: $(".about").offset().top
+        -125}, 800);
+    });
